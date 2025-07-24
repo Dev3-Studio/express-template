0c101680af6b9b24d2de25fa2a31e937d882891e
ColourfulMelon <jordynnitch@gmail.com>
2025-07-24 17:20:38 +0200

    update packages

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,14 +9,14 @@ const envSchema = z.object({
     DB_NAME: z.string(),
     DB_CERT: z.string().optional(),
     DB_CERT_PATH: z.string().optional().default('./ca-certificate.crt'),
-    ALLOWED_ORIGINS: z.preprocess((val) => String(val).split(','), z.array(z.string().url())),
+    ALLOWED_ORIGINS: z.preprocess((val) => String(val).split(','), z.array(z.url())),
     PORT: z.coerce.number().int().min(1).max(65535).default(5000),
     NODE_ENV: z.enum(['development', 'production']).default('development'),
     
-    BACKEND_DOMAIN: z.string().url().default('http://localhost:5000'),
+    BACKEND_DOMAIN: z.url().default('http://localhost:5000'),
 
     BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string().url(),
+    BETTER_AUTH_URL: z.url(),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
     TWITTER_CLIENT_ID: z.string(),
